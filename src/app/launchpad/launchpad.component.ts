2a8590eff2f6a8fe38b20cda05f01d9d2fa5252b
Bruce Grodnik <info@oncalldev.com>
2018-10-03 17:15:58 -0500

    Initial Checkup

---
+++ src/app/launchpad/launchpad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-launchpad',
+  templateUrl: './launchpad.component.html',
+  styleUrls: ['./launchpad.component.css']
+})
+export class LaunchpadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
